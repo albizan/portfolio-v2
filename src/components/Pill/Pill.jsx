1c006f168e2d7c490310bf1c8fa630295e62b208
unknown <albizan@hotmail.it>
2020-04-14 16:09:51 +0200

    Fixed card height

--- src/components/Pill/Pill.jsx
+++ src/components/Pill/Pill.jsx
@@ -3,7 +3,7 @@ import { PillWrapper } from './styled';
 
 const Pill = ({ value }) => {
   return (
-    <PillWrapper className="inline-block leading-none text-xxs uppercase font-bold tracking-tighter rounded-full mb-2">
+    <PillWrapper className="inline-block leading-none text-xxs uppercase font-black tracking-widest rounded-full mb-2">
       {value}
     </PillWrapper>
   );
