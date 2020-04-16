7c9f049fab83eb7e454a6a0a4fc26a72c3991f5c
unknown <albizan@hotmail.it>
2020-04-16 15:50:41 +0200

    Create LinkButton

--- src/pages/Projectpage/styled.js
+++ src/pages/Projectpage/styled.js
@@ -1,1 +1,15 @@
 import styled from 'styled-components';
+
+export const LinkButton = styled.a`
+  display: inline-block;
+  border: 2px solid ${(props) => props.theme.accent};
+  color: ${(props) => props.theme.accent};
+  border-radius: 8px;
+  text-transform: uppercase;
+  font-weight: bold;
+
+  &:hover {
+    color: ${(props) => '#FFF'};
+    background-color: ${(props) => props.theme.accent};
+  }
+`;
