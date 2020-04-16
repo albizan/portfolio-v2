57d7c0a755297b624c7485ab1026e5b0a02e2902
unknown <albizan@hotmail.it>
2020-04-16 15:50:33 +0200

    Render title and linkButtons

--- src/pages/Projectpage/Projectpage.jsx
+++ src/pages/Projectpage/Projectpage.jsx
@@ -1,31 +1,49 @@
 import React from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
+import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
+import { LinkButton } from './styled';
 
 const Projectpage = (props) => {
-  console.log(props.match, props.location);
+  const { t } = useTranslation();
+  const history = useHistory();
   const { project } = props.location.state;
   return (
-    <div className="h-screen w-full">
-      <div className="container px-4 sm:px-6 md:px-8 mx-auto">
-        <div className="h-32 mt sm:pt-4 flex items-center">
-          <FiArrowLeft className="inline-block text-3xl" />
-          <span className="font-bold">Home</span>
-        </div>
-        <div>
-          <h1>{project.title}</h1>
-        </div>
-        <div className="h-64 sm:flex bg-green-300">
-          <div className="sm:w-1/3 bg-red-300">
-            <img
-              src={project.img}
-              alt="project - screenshot"
-              className="h-full object-cover object-center"
-            />
+    <div className="h-screen w-full px-4 pt-4">
+      <section className="container mx-auto">
+        <div className="go-back-wrapper flex items-center h-20 pt-2">
+          <div onClick={() => history.goBack()} className="go-back flex items-center">
+            <FiArrowLeft className="text-4xl inline-block" />
+            <span className="ml-1 font-bold text-xl leading-none tracking-tighter">Go Back</span>
           </div>
-          <div className="w-2/3">{project.description}</div>
         </div>
-      </div>
+        <h1 className="mt-5 font-bold text-2xl">{t(project.title)}</h1>
+        <div className="flex justify-start items-center">
+          <div className="link buttons">{renderLinks(project.links)}</div>
+          <div className="link buttons">{renderRepos(project.repos)}</div>
+        </div>
+      </section>
     </div>
   );
 };
+
+function renderLinks(links) {
+  return links.map((link) => {
+    return (
+      <LinkButton target="_blank" key={link.url} className="text-xs py-1 px-2 mr-1" href={link.url}>
+        {link.label}
+      </LinkButton>
+    );
+  });
+}
+
+function renderRepos(repos) {
+  return repos.map((repo) => {
+    return (
+      <LinkButton target="_blank" key={repo.url} className="text-xs py-1 px-2 mr-1" href={repo.url}>
+        {repo.label}
+      </LinkButton>
+    );
+  });
+}
 export default Projectpage;
