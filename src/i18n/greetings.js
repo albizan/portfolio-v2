72fe523048304f1a824316424d16d5cc736744fd
unknown <albizan@hotmail.it>
2020-04-26 21:47:25 +0200

    Completed english translation

--- src/i18n/greetings.js
+++ src/i18n/greetings.js
@@ -3,10 +3,20 @@ const greetings_title = {
   en: 'Hey! Nice to meet you',
 };
 const greetings_text_1 = {
-  it:
-    "Benvenuto nel mio sito web, sono contento che tu abbia visitato il mio spazio personale su internet. Qui è dove sono orgoglioso di mostrare le mie capacità, i miei progetti, le mie esperienze e le mie speranze future nel campo dello sviluppo web. Sono un geek di 24 anni che si trova nella bellissima Torino. Ho conseguito la laurea in Ingegneria Informatica presso il Politecnico di Torino. Amo le nuove tecnologie web, sia sul front-end che sul back-end. Attualmente sto studiando per diventare uno sviluppatore full stack con React e Node. Come web developer cerco di costruire applicazioni software che gli utenti apprezzino: sicure, veloci, facile da usare e da capire con un'interfaccia utente pulita ed elegante. Sono tenace, curioso, disciplinato e lavoro duramente e continuamente per migliorare le mie capacità e per imparare le nuove tecnologie di mio interesse.",
-  en:
-    "Welcome to my website, I am glad you visited my personal space, here is where I am proud to show my skills, projects, past experiences and future hopes in the web development field. I am a 23-year-old geek located in beautiful Turin, Italy. I got my bachelor degree in Computer Engineering at the Politecnico di Torino. I love new web technologies, both on the front-end and on the back-end. I am currently studying to become a full stack developer with React and Node. As a web developer, officialy I make websites and design restful APIs. But really? Well, I try to build software users appreciate: secure, fast, easy to use and understand with a clean and elegant user interface. I'm persistent, curious and continuously working on improving my capabilities",
+  it: `Benvenuto nel mio sito web, sono contento che tu abbia visitato il mio 
+  spazio personale su internet. Qui è dove sono orgoglioso di mostrare le 
+  mie capacità, i miei progetti, le mie esperienze e le mie speranze future nel 
+  campo dello sviluppo web. Sono un geek di 24 anni che abita nella 
+  bellissima Torino. Ho conseguito la laurea in Ingegneria Informatica presso 
+  il Politecnico di Torino. Amo le nuove tecnologie web, sia per quanto riguarda
+  il front-end che il back-end. Attualmente sto studiando per diventare uno 
+  sviluppatore full stack con React e Node. Come web developer cerco di costruire 
+  applicazioni software che gli utenti apprezzino: sicure, veloci, facili da 
+  utilizzare e da capire, con un'interfaccia utente pulita ed elegante. Sono 
+  tenace, curioso, disciplinato e lavoro duramente e continuamente per 
+  migliorare le mie capacità e per imparare nuove tecnologie che siano di mio 
+  interesse.`,
+  en: `Welcome to my website. I’m happy that you’ve visited my personal space on the internet. This is where I’m proud to show my abilities, my projects, my experiences and my future hopes in the field of web development. I’m a 24 year old geek who lives in beautiful Turin. I graduated in Computer Engineering at the Polytechnic University of Turin. I love new web technologies, both front-end and back-end. I’m currently studying to become a full stack developer with React and Node. As a web developer, I’m looking to build software applications that users appreciate: safe, quick, easy to use and understand, with a clean and elegant user interface. I’m determined, curious, disciplined and work hard and continuously to improve my abilities to learn new technologies I’m interested in.`,
 };
 
 export const greetings_translation_en = {
