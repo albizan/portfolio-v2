35ebe83f814464d6e2fc20fb26bc17a01c09a9a8
Alberto Zanotti <albizan@hotmail.it>
2020-10-04 16:51:45 +0200

    Update age

--- src/i18n/greetings.js
+++ src/i18n/greetings.js
@@ -6,7 +6,7 @@ const greetings_text_1 = {
   it: `Benvenuto nel mio sito web, sono contento che tu abbia visitato il mio 
   spazio personale su internet. Qui è dove sono orgoglioso di mostrare le 
   mie capacità, i miei progetti, le mie esperienze e le mie speranze future nel 
-  campo dello sviluppo web. Sono un geek di 24 anni che abita nella 
+  campo dello sviluppo web. Sono un geek di 25 anni che abita nella 
   bellissima Torino. Ho conseguito la laurea in Ingegneria Informatica presso 
   il Politecnico di Torino. Amo le nuove tecnologie web, sia per quanto riguarda
   il front-end che il back-end. Attualmente sto studiando per diventare uno 
@@ -16,7 +16,7 @@ const greetings_text_1 = {
   tenace, curioso, disciplinato e lavoro duramente e continuamente per 
   migliorare le mie capacità e per imparare nuove tecnologie che siano di mio 
   interesse.`,
-  en: `Welcome to my website. I’m happy that you’ve visited my personal space on the internet. This is where I’m proud to show my abilities, my projects, my experiences and my future hopes in the field of web development. I’m a 24 year old geek who lives in beautiful Turin. I graduated in Computer Engineering at the Polytechnic University of Turin. I love new web technologies, both front-end and back-end. I’m currently studying to become a full stack developer with React and Node. As a web developer, I’m looking to build software applications that users appreciate: safe, quick, easy to use and understand, with a clean and elegant user interface. I’m determined, curious, disciplined and work hard and continuously to improve my abilities to learn new technologies I’m interested in.`,
+  en: `Welcome to my website. I’m happy that you’ve visited my personal space on the internet. This is where I’m proud to show my abilities, my projects, my experiences and my future hopes in the field of web development. I’m a 25 year old geek who lives in beautiful Turin. I graduated in Computer Engineering at the Polytechnic University of Turin. I love new web technologies, both front-end and back-end. I’m currently studying to become a full stack developer with React and Node. As a web developer, I’m looking to build software applications that users appreciate: safe, quick, easy to use and understand, with a clean and elegant user interface. I’m determined, curious, disciplined and work hard and continuously to improve my abilities to learn new technologies I’m interested in.`,
 };
 
 export const greetings_translation_en = {
